56a71df7e5f0529b986eaceb449b83700fabf504
Bruce-Mahagwa <jacobbruce880@gmail.com>
2024-08-09 23:44:42 +0300

    Adds styling to main container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,59 @@ import "./normalize.css"
 // components
 import Button from './Components/Button/Button';
 import ListItem from './Components/ListItem/ListItem';
-
+// hooks
+import { useState } from 'react';
 function App() {
+  const [checkBoxStyles, setCheckBoxStyles] = useState([])
+  const [largeContainerStyles, setLargeContainerStyles] = useState([])
+  function onMouseUp(e) {
+      setCheckBoxStyles((prev) => {
+          return ""
+      })
+      setLargeContainerStyles((prev) => {
+          return (
+              ""
+          )
+      })
+  }
+
   return (
     <>
-    <main className = "main_container">
-      {/* <Button /> */}
-      <form>
-              <ListItem />
-            </form>
+    <main className = "main_container"
+    onMouseMove = {onMouseUp}
+    // we set an onMouseMove handler incase the user drags the mouse accross several list element
+    >
+      <form className = "form">
+        <ListItem 
+        checkBoxStyles = {checkBoxStyles}
+        setCheckBoxStyles ={setCheckBoxStyles}
+        largeContainerStyles={largeContainerStyles}
+        setLargeContainerStyles={setLargeContainerStyles}
+        onMouseUp = {onMouseUp}
+        />
+        <ListItem 
+        checkBoxStyles = {checkBoxStyles}
+        setCheckBoxStyles ={setCheckBoxStyles}
+        largeContainerStyles={largeContainerStyles}
+        setLargeContainerStyles={setLargeContainerStyles}
+        onMouseUp = {onMouseUp}
+        />
+        <ListItem 
+        checkBoxStyles = {checkBoxStyles}
+        setCheckBoxStyles ={setCheckBoxStyles}
+        largeContainerStyles={largeContainerStyles}
+        setLargeContainerStyles={setLargeContainerStyles}
+        onMouseUp = {onMouseUp}
+        />
+        <ListItem 
+        checkBoxStyles = {checkBoxStyles}
+        setCheckBoxStyles ={setCheckBoxStyles}
+        largeContainerStyles={largeContainerStyles}
+        setLargeContainerStyles={setLargeContainerStyles}
+        onMouseUp = {onMouseUp}
+        />
+        <Button />
+      </form>
     </main>
     </>
   )
--- frontend/src/Components/ListItem/ListItem.js
+++ frontend/src/Components/ListItem/ListItem.js
@@ -5,34 +5,66 @@ import { BsCheck2 } from "react-icons/bs";
 import { IconContext } from "react-icons";
 // hooks
 import { useState } from "react";
-const ListItem = () => {
-    const [checkBoxStyles, setCheckBoxStyles] = useState([])
-    function onMouseDown() {
+const ListItem = ({checkBoxStyles, setCheckBoxStyles, largeContainerStyles, setLargeContainerStyles, onMouseUp}) => {
+
+    const [isItemChecked, setIsItemChecked] = useState(false);
+    function onInputChecked(e) {    
+        // captures the checkbox element and checks if it is checked
+        try {
+            if (e.currentTarget) {
+                const element = e.currentTarget?.childNodes[1]?.childNodes[0];
+                const isChecked = element?.checked;
+                if (isChecked) {
+                    setIsItemChecked(true)
+                    return true;
+                }
+            }
+        }
+        catch(e) {
+            setIsItemChecked(false)
+            return false;
+        }
+        setIsItemChecked(false)
+        return false;
+    }
+
+    function onMouseDown(e) {
         // describes the styles and classes when a user presses the mouse on the checkbox element
         setCheckBoxStyles((prev) => {
+            if (isItemChecked) {
+                // avoids the box shadow styling when checkbox is being unchecked
+                return "show_tick_mark"
+            }
             return (
-                "show_tick_mark"
+                "show_tick_mark box-shadow"
+            )
+        })
+        setLargeContainerStyles((prev) => {
+            return (
+                "border-color-focus"
             )
         })
     }
-    function onMouseUp() {
-        setCheckBoxStyles((prev) => {
-            return ""
-        })
-    }
+    
+
     return (
-        <li className = "list_item">
-            <label className = "page_name" htmlFor = "all_pages">All Pages</label>
+        <li className = {`list_item`}
+        onMouseDown={onMouseDown}
+        onMouseUp = {onMouseUp}
+        onChange = {onInputChecked}
+        onClick={onInputChecked}
+        >
+        <label className = "page_name" 
+            htmlFor = "all_pages"
+            >All Pages</label>
             <div className = "check_page">
                 <input type = "checkbox" name = "all_pages" className = {`checkbox ${checkBoxStyles}`} 
                 id = "all_pages" 
-                onMouseDown={onMouseDown}
-                onMouseUp = {onMouseUp}
-                />
+                 />
                 <IconContext.Provider value = {{className: "tick_icon center"}}>
                     <BsCheck2 />
                 </IconContext.Provider>
-                <div className = "check_page_container_large center">
+                <div className = {`check_page_container_large center ${largeContainerStyles}`}>
                     <div className = "check_page_container_small center">
 
                     </div>
