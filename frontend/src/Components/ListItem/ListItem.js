9ab055b6383a76fce56a3cbfcbb17d357c7bf755
Bruce-Mahagwa <jacobbruce880@gmail.com>
2024-08-10 10:39:54 +0300

    Adds a background to checkbox when pressing

--- frontend/src/Components/ListItem/ListItem.js
+++ frontend/src/Components/ListItem/ListItem.js
@@ -6,7 +6,7 @@ import { BsCheck2 } from "react-icons/bs";
 import { IconContext } from "react-icons";
 // hooks
 
-const ListItem = ({checkBoxStyles, largeContainerStyles, setLargeContainerStyles, onMouseUp, htmlFor, label, height=41}) => {
+const ListItem = ({largeContainerStyles, setLargeContainerStyles, onMouseUp, htmlFor, label, height=41}) => {
     
     function onMouseDown(e) {
         // describes the styles and classes when a user presses the mouse on the checkbox element
@@ -28,14 +28,30 @@ const ListItem = ({checkBoxStyles, largeContainerStyles, setLargeContainerStyles
     }
     function isCheckBoxChecked(e) {
         const element = e.currentTarget?.nextElementSibling?.childNodes[0];
-        if (element) {
-            if (element.checked) {
+        const element2 = e.currentTarget?.parentElement?.childNodes[0]
+        if (element || element2) {
+            if (element?.checked) {
+                console.log("on press")
                 element.classList.add("blue-background-checked")
-                return "";
+            }
+            else if (element2?.checked) {
+                element2.classList.add("blue-background-checked")
+        }
+    }
+}
+    function removeCheckBox(e) {
+        const element = e.currentTarget?.nextElementSibling?.childNodes[0];
+        const element2 = e.currentTarget?.parentElement?.childNodes[0]
+        if (element || element2) {
+            if (element?.checked) {
+                element.classList.remove("blue-background-checked")
+            }
+            else if (element2?.checked) {
+                element2.classList.remove("blue-background-checked")
             }
         }
-        return "";
     }
+    
     return (
         <li className = {`list_item`}
         onMouseDown={onMouseDown}
@@ -45,13 +61,16 @@ const ListItem = ({checkBoxStyles, largeContainerStyles, setLargeContainerStyles
         <label className = "page_name" 
             htmlFor = {htmlFor}
             onMouseDown={isCheckBoxChecked}
+            onMouseUp = {removeCheckBox}
             >{label}</label>
             <div className = "check_page">
                 <input type = "checkbox" name = {htmlFor} className = {`checkbox center`} 
                 id = {htmlFor} 
                  />
                 <IconContext.Provider value = {{className: "tick_icon center"}}>
-                    <label htmlFor = {htmlFor}>
+                    <label htmlFor = {htmlFor}
+                    onMouseDown={isCheckBoxChecked}
+                    onMouseUp = {removeCheckBox}>
                         <BsCheck2  />
                     </label>
                 </IconContext.Provider>
